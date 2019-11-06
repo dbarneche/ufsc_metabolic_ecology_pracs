22d92d4a38b5f7bbfc032d6bcf03edcd6a17d7ac
Diego Barneche <diego.barneche@ymail.com>
2019-11-06 15:08:26 -0300

    added code for temperature dependence

--- analyses.R
+++ analyses.R
@@ -28,3 +28,38 @@ for (i in 1:nrow(valores)) {
     lines(ranges_i, preds_i, col = 'dodgerblue2', lwd = 0.8, lty = 2)
 }
 lines(ranges, preds, col = 'tomato', lwd = 3, lty = 2)
+
+
+k                         <-  8.62e-5
+resp$temperature_kelvin   <-  resp$temperature_celsius + 273.15
+resp$inverse_temperature  <-  1 / (k * resp$temperature_kelvin)
+resp$lnMass_g             <-  log(resp$individual_mass_g)
+resp$lnResp_rate_gC_per_d <-  log(resp$respiration_rate_gC_per_d)
+
+complete_model  <-  lm(lnResp_rate_gC_per_d ~ lnMass_g + inverse_temperature, data = resp)
+summary(complete_model)
+
+# estatistcamente a equação tem forma:
+# lnResp_rate_gC_per_d ~ lnMass_g + inverse_temperature
+# mas matematicamente, o equivalente é na verdade:
+# lnResp_rate_gC_per_d = ln(c) + alpha * lnMass_g + Ea * inverse_temperature
+# notando que os parâmetros ln(c), alpha e Ea correspondem respectivamente ao intercepto, slope do lnMass_g e slope da temperatura inversa.
+
+parametros  <-  coef(complete_model)
+
+parametros[2]
+alpha  <-  parametros['lnMass_g'] # alpha
+Ea     <-  parametros['inverse_temperature'] # Ea
+
+
+resp$mass_cor_resp_rate_gC_per_d_g_alpha  <-  resp$respiration_rate_gC_per_d / (resp$individual_mass_g ^ alpha)
+resp$temp_cor_resp_rate_gC_per_d_g_alpha  <-  resp$respiration_rate_gC_per_d / exp(resp$inverse_temperature * Ea)
+
+plot(respiration_rate_gC_per_d ~ inverse_temperature, data = resp, log = 'y')
+plot(mass_cor_resp_rate_gC_per_d_g_alpha ~ inverse_temperature, data = resp, log = 'y')
+
+plot(respiration_rate_gC_per_d ~ lnMass_g, data = resp, log = 'y')
+
+plot(temp_cor_resp_rate_gC_per_d_g_alpha ~ lnMass_g, data = resp, log = 'y')
+10^0.75
+exp(1/k * (-Ea/303 + Ea/294))
